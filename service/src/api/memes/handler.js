d1edb80f6deda8675fc082d1d0f35ed0b3db50bf
Karol Jaksik <jaksikkarol@gmail.com>
2020-04-26 17:45:45 +0200

    add crud

--- service/src/api/memes/handler.js
+++ service/src/api/memes/handler.js
@@ -1,19 +1,54 @@
+import fs from 'fs'
+import path from 'path';
+const __dirname = path.resolve();
+
 const createMeme = (req, res) => {
     res.send("create meme")
 }
 
 const getMeme = (req, res) => {
-    res.send("get meme")
+    
+    let image =path.join(__dirname,'memes', req.params.id + '.jpg')
+
+    fs.readFile(image,(err, data)=>{
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'image/jpeg'})
+        res.end(data);
+    });
+
+    
+}
+
+const listMemes = (req, res) => {
+
+   let dir = path.join(__dirname,'memes')
+  
+    fs.readdir(dir, (err,files)=>{
+        if (err) throw err;
+       files = files.map(file => file.replace('.jpg',''))
+        res.send(files)
+    }) 
 }
 
 const deleteMeme = (req, res) => {
-    res.send("delete meme")
+
+    let image =path.join(__dirname,'memes', req.params.id + '.jpg')
+
+
+    fs.stat(image, (err,stats)=>{
+        if (err) throw err;
+        fs.unlink(image,(err)=>{
+            res.end(req.params.id);
+        })
+    })
+
 }
 
 const MemesHandler = {
     createMeme: createMeme,
     getMeme: getMeme,
-    deleteMeme: deleteMeme
+    deleteMeme: deleteMeme,
+    listMemes: listMemes
 };
 
-export default MemesHandler
+export default MemesHandler;
--- service/src/api/routes.js
+++ service/src/api/routes.js
@@ -4,8 +4,9 @@ import handler from './memes/handler.js'
 
 const routes = Router();
 
-routes.get('/memes', handler.getMeme)
+routes.get('/memes/:id', handler.getMeme)
+routes.get('/memes',handler.listMemes)
 routes.post('/memes', handler.createMeme)
-routes.delete('/memes', handler.deleteMeme)
+routes.delete('/memes/:id', handler.deleteMeme)
 
-export default routes
+export default routes;
